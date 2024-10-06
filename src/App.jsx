d95671e5e1a12a39489c86842a184d18d1744555
Joy Tarafder <67510333+JoyTarafder@users.noreply.github.com>
2024-10-06 12:57:28 +0600

    all section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import ClientSection from "./components/ClientSection";
+import ContactSection from "./components/ContactSection";
 import FeatureSection from "./components/FeatureSections";
 import Footer from "./components/Footer";
 import GetStartSection from "./components/GetStartSection";
 import HeroSection from "./components/HeroSection";
 import InnovationSection from "./components/InnovativeSection";
 import PropertiesSection from "./components/PropertiesSection";
+import QuestionSection from "./components/QuestionSection";
 import "./index.css";
 
 export default function App() {
@@ -14,7 +17,9 @@ export default function App() {
       <PropertiesSection />
       <InnovationSection />
       <GetStartSection />
-
+      <QuestionSection/>
+      <ClientSection/>
+      <ContactSection />
       <Footer />
     </>
   );
